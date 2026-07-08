d14679b379813d13f5f7beba0faf63f0e14b0c35
Amartya Bhaumik <155053095+CoolAnt001@users.noreply.github.com>
2026-07-08 23:01:12 +0530

    Update HW1.java

--- HW1.java
+++ HW1.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class HW1{
 	    public static void main(String[] args) {
 	        Scanner sc = new Scanner(System.in);
-
+ 
 	        System.out.print("Enter a: ");
 	        int a = sc.nextInt();
 
